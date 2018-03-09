a3da1a9e88c1481aa25f97f75186ecee18e26cec
Divya Gollapudi <dgollapudi@williamhill.co.uk>
2018-03-09 14:29:07 +0000

    Initial commit

---
+++ src/test/scala/BetsTest/scenarios/ScnSportsbookBets.scala
@@ -0,0 +1,12 @@
+package test.scala.BetsTest.scenarios
+import io.gatling.core.Predef._
+
+/**
+  * Created by dgollapudi on 09/03/2018.
+  */
+object ScnSportsbookBets {
+
+  val scn_sbBrowsing = scenario("SB: Browsing")
+
+
+}
---
+++ src/test/scala/BetsTest/simulations/SimSportsBookBets.scala
@@ -0,0 +1,8 @@
+package BetsTest.simulations
+
+/**
+  * Created by dgollapudi on 09/03/2018.
+  */
+class SimSportsBookBets {
+
+}
